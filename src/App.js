08124e8c0e676752b952fa68618dc8c30f4e9138
Ali Asanov <ali.asanov@cd.p2h.com>
2023-10-02 19:38:01 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import './App.css';
 import { useState } from 'react';
 
+const QUANTITIES = [5, 10, 99, 100]
+
 function App() {
   const [counter, setCounter] = useState(0)
+  const [maxCounterNum, setMaxCounterNum] = useState(QUANTITIES[0])
 
-  const increment = () => setCounter(prev => prev + 1)
+  const isMax = counter === maxCounterNum
+
+  const increment = () => {
+    if (counter === maxCounterNum) return
+    setCounter(prev => prev + 1)
+  }
   const reset = () => setCounter(0)
+
+  const handleQuantity = (quantity) => () => {
+    setMaxCounterNum(quantity)
+  }
 
   return (
     <div className="App">
-      <div className='counter' onClick={increment}>
+      <div className={`counter ${isMax && 'max'}`} onClick={increment}>
         <p className='number'>{counter}</p>
       </div>
-      <button type='button' className='resetBtn' onClick={reset}>Reset</button>
+      <div className='btnsWrapper'>
+        <button type='button' className='resetBtn btn' onClick={reset}>Reset</button>
+        <div className='quantityBtns'>
+          {QUANTITIES.map(quantity => 
+            <button 
+              key={quantity} 
+              type='button' 
+              className={`quantityBtn btn ${quantity === maxCounterNum && 'active'}`} 
+              onClick={handleQuantity(quantity)}
+            >
+              {quantity}
+            </button>)}
+        </div>
+      </div>
     </div>
   );
 }

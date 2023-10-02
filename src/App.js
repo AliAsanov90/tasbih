f0cc9f60835836950a02847a75656f9643e7d834
Ali Asanov <ali.asanov@cd.p2h.com>
2023-10-02 20:21:23 +0300

    update

--- src/App.js
+++ src/App.js
@@ -13,6 +13,16 @@ function App() {
     if (counter >= maxCounterNum) return
     setCounter(prev => prev + 1)
   }
+
+  const decrement = () => {
+    if (counter === 0) return
+    if (counter > maxCounterNum) {
+      setCounter(maxCounterNum)
+      return
+    }
+    setCounter(prev => prev - 1)
+  }
+
   const reset = () => setCounter(0)
 
   const handleQuantity = (quantity) => () => {
@@ -21,23 +31,26 @@ function App() {
 
   return (
     <div className="App">
-      <div className={`counter ${isMax && 'max'}`} onClick={increment}>
-        <p className='number'>{counter}</p>
-      </div>
-      <div className='btnsWrapper'>
-        <button type='button' className='resetBtn btn' onClick={reset}>Reset</button>
-        <div className='quantityBtns'>
-          {QUANTITIES.map(quantity => 
-            <button 
-              key={quantity} 
-              type='button' 
-              className={`quantityBtn btn ${quantity === maxCounterNum && 'active'}`} 
-              onClick={handleQuantity(quantity)}
-            >
-              {quantity}
-            </button>)}
+      <div className='minus' onClick={decrement}><span>&mdash;</span></div>       
+      <div>
+        <div className={`counter ${isMax && 'max'}`} onClick={increment}>
+          <p className='number'>{counter}</p>
         </div>
-      </div>
+        <div className='btnsWrapper'>
+          <button type='button' className='resetBtn btn' onClick={reset}>Reset</button>
+          <div className='quantityBtns'>
+            {QUANTITIES.map(quantity => 
+              <button 
+                key={quantity} 
+                type='button' 
+                className={`quantityBtn btn ${quantity === maxCounterNum && 'active'}`} 
+                onClick={handleQuantity(quantity)}
+              >
+                {quantity}
+              </button>)}
+          </div>
+        </div>
+      </div> 
     </div>
   );
 }

950dc626a08b7c3fa9a544cee1a0c675c108c8e7
Ali Asanov <ali.asanov@cd.p2h.com>
2024-10-12 23:56:12 +0300

    add Unlimited quantity

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import './App.css';
 import { useState } from 'react';
 
-const QUANTITIES = [7, 33, 99, 100]
+const QUANTITIES = [
+  { title: '7', value: 7 }, 
+  { title: '33', value: 33 }, 
+  { title: '99', value: 99 }, 
+  { title: '100', value: 100 }, 
+  { title: 'Unlimited', value: 999999999 }
+]
+
+const isUnlimitedQuantityValue = QUANTITIES.find(({ title }) => title === 'Unlimited').value || 999999999
 
 function App() {
-  const [counter, setCounter] = useState(0)
-  const [maxCounterNum, setMaxCounterNum] = useState(QUANTITIES[1])
+  const [counter, setCounter] = useState(99)
+  const [maxCounterNum, setMaxCounterNum] = useState(isUnlimitedQuantityValue)
 
   const isMax = counter >= maxCounterNum
 
   const increment = () => {
     if (counter >= maxCounterNum) return
+
     setCounter(prev => prev + 1)
   }
 
   const decrement = () => {
     if (counter === 0) return
+
     if (counter > maxCounterNum) {
       setCounter(maxCounterNum)
       return
     }
+
     setCounter(prev => prev - 1)
   }
 
@@ -39,14 +50,14 @@ function App() {
         <div className='btnsWrapper'>
           <button type='button' className='resetBtn btn' onClick={reset}>Reset</button>
           <div className='quantityBtns'>
-            {QUANTITIES.map(quantity => 
+            {QUANTITIES.map(({ title, value }) => 
               <button 
-                key={quantity} 
+                key={title} 
                 type='button' 
-                className={`quantityBtn btn ${quantity === maxCounterNum && 'active'}`} 
-                onClick={handleQuantity(quantity)}
+                className={`quantityBtn btn ${value === maxCounterNum && 'active'}`} 
+                onClick={handleQuantity(value)}
               >
-                {quantity}
+                {title}
               </button>)}
           </div>
         </div>

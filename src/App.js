96bc70b810b4f2c80e907999686bd50ab5ecc638
Ali Asanov <ali.asanov@cd.p2h.com>
2025-07-23 19:59:56 +0300

    add counter value to local storage

--- src/App.js
+++ src/App.js
@@ -1,69 +1,82 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css"
+import { useEffect, useState } from "react"
 
 const QUANTITIES = [
-  { title: '7', value: 7 }, 
-  { title: '33', value: 33 }, 
-  { title: '99', value: 99 }, 
-  { title: '100', value: 100 }, 
-  { title: 'Unlimited', value: 999999999 }
+  { title: "33", value: 33 },
+  { title: "100", value: 100 },
+  { title: "1000", value: 1000 },
+  { title: "2000", value: 2000 },
+  { title: "Unlimited", value: 999999999 },
 ]
 
-const isUnlimitedQuantityValue = QUANTITIES.find(({ title }) => title === 'Unlimited').value || 999999999
+const LOCAL_STORAGE_KEY = "counter"
+
+const isUnlimitedQuantityValue =
+  QUANTITIES.find(({ title }) => title === "Unlimited").value ||
+  QUANTITIES[QUANTITIES.length - 1].value
 
 function App() {
   const [counter, setCounter] = useState(0)
   const [maxCounterNum, setMaxCounterNum] = useState(isUnlimitedQuantityValue)
-
   const isMax = counter >= maxCounterNum
+
+  const saveCounterValue = (value) => {
+    setCounter(value)
+    localStorage.setItem(LOCAL_STORAGE_KEY, String(value))
+  }
 
   const increment = () => {
     if (counter >= maxCounterNum) return
-
-    setCounter(prev => prev + 1)
+    saveCounterValue(counter + 1)
   }
 
   const decrement = () => {
     if (counter === 0) return
-
-    if (counter > maxCounterNum) {
-      setCounter(maxCounterNum)
-      return
-    }
-
-    setCounter(prev => prev - 1)
+    if (counter > maxCounterNum) return saveCounterValue(maxCounterNum)
+    saveCounterValue(counter - 1)
   }
 
-  const reset = () => setCounter(0)
+  const reset = () => saveCounterValue(0)
 
-  const handleQuantity = (quantity) => () => {
-    setMaxCounterNum(quantity)
-  }
+  const handleQuantity = (quantity) => () => setMaxCounterNum(quantity)
+
+  useEffect(() => {
+    const storedCounter = Number(localStorage.getItem(LOCAL_STORAGE_KEY))
+
+    if (storedCounter) setCounter(storedCounter)
+  }, [])
 
   return (
     <div className="App">
-      <div className='minus' onClick={decrement}><span>&mdash;</span></div>       
-      <div>
-        <div className={`counter${isMax ? ' max' : ''}`} onClick={increment}>
-          <p className='number'>{counter}</p>
+      <div className="minus" onClick={decrement}>
+        <span>&mdash;</span>
+      </div>
+      <div className="counter-wrapper">
+        <div className={`counter${isMax ? " max" : ""}`} onClick={increment}>
+          <p className="number">{counter}</p>
         </div>
-        <div className='btnsWrapper'>
-          <button type='button' className='resetBtn btn' onClick={reset}>Reset</button>
-          <div className='quantityBtns'>
-            {QUANTITIES.map(({ title, value }) => 
-              <button 
-                key={title} 
-                type='button' 
-                className={`quantityBtn btn ${value === maxCounterNum && 'active'}`} 
+        <div className="btnsWrapper">
+          <button type="button" className="resetBtn btn" onClick={reset}>
+            Reset
+          </button>
+          <div className="quantityBtns">
+            {QUANTITIES.map(({ title, value }) => (
+              <button
+                key={title}
+                type="button"
+                className={`quantityBtn btn ${
+                  value === maxCounterNum && "active"
+                }`}
                 onClick={handleQuantity(value)}
               >
                 {title}
-              </button>)}
+              </button>
+            ))}
           </div>
         </div>
-      </div> 
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
